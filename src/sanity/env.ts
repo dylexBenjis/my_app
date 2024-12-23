cfbe2254b69df95d807241c4a94172e7546cf288
dylexBnejis <benjichukwudile@gmail.com>
2024-12-23 20:23:46 +0100

    update

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-09-30";
 
 export const dataset = assertValue(
-   process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET,
+  "production",
   "Missing environment variable: SANITY_STUDIO_DATASET or NEXT_PUBLIC_SANITY_STUDIO_DATASET"
 );
 
 export const projectId = assertValue(
-   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "0ntidcrl" ,
   "Missing environment variable: SANITY_STUDIO_PROJECT_ID or NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
